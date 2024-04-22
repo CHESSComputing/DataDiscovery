ea6b5dd3c9108d909942f80d6720ddc1cf659a12
Valentin Kuznetsov <vkuznet@gmail.com>
2024-04-22 09:19:47 -0400

    Add QLManager initialization

--- server.go
+++ server.go
@@ -5,13 +5,17 @@ package main
 // Copyright (c) 2023 - Valentin Kuznetsov <vkuznet@gmail.com>
 //
 import (
+	"log"
+
 	srvConfig "github.com/CHESSComputing/golib/config"
+	ql "github.com/CHESSComputing/golib/ql"
 	server "github.com/CHESSComputing/golib/server"
 	services "github.com/CHESSComputing/golib/services"
 	"github.com/gin-gonic/gin"
 )
 
 var _httpReadRequest *services.HttpRequest
+var _qlMgr ql.QLManager
 var Verbose int
 
 // helper function to setup our router
@@ -28,6 +32,11 @@ func setupRouter() *gin.Engine {
 func Server() {
 	Verbose = srvConfig.Config.Discovery.WebServer.Verbose
 	_httpReadRequest = services.NewHttpRequest("read", Verbose)
+
+	// initialize QL manager
+	if err := _qlMgr.Init(srvConfig.Config.QL.ServiceMapFile); err != nil {
+		log.Println("ERROR: FOXDEN QL Manager is not initialized", err)
+	}
 
 	// setup web router and start the service
 	r := setupRouter()
